c8c7d03680ed76663c3e32608183175fcf69cccd
Marcelina <marcelina.hasiak@gmail.com>
2025-01-11 16:37:27 +0100

    feat: add fetch logger

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	experimental: {
+		logging: {
+			level: "verbose",
+			fullUrl: true,
+		},
+	},
+};
 
 module.exports = nextConfig;
